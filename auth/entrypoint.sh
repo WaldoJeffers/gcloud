e585a3dfe2a8074e6b0c620274bbe92264e25de1
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-16 19:14:42 +0200

    fix: Use double quotes

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo "$GCLOUD_AUTH" | base64 --decode > "$GITHUB_WORKSPACE"/gcloud.json
-export GOOGLE_APPLICATION_CREDENTIALS=$GITHUB_WORKSPACE/gcloud.json
+export GOOGLE_APPLICATION_CREDENTIALS="$GITHUB_WORKSPACE/gcloud.json"
 sh -c "gcloud auth activate-service-account --key-file=$GITHUB_WORKSPACE/gcloud.json $*"
