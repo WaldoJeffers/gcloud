b2f4aff3df6e52f1fab7ca2502577c0710b84271
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-17 15:15:40 +0200

    rm setenv

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "$GCLOUD_AUTH" | base64 --decode > "$GITHUB_WORKSPACE"/gcloud.json
-setenv GOOGLE_APPLICATION_CREDENTIALS "$GITHUB_WORKSPACE/gcloud.json"
+#setenv GOOGLE_APPLICATION_CREDENTIALS "$GITHUB_WORKSPACE/gcloud.json"
 export GOOGLE_APPLICATION_CREDENTIALS="$GITHUB_WORKSPACE/gcloud.json"
 sh -c "gcloud auth activate-service-account --key-file=$GITHUB_WORKSPACE/gcloud.json $*"
