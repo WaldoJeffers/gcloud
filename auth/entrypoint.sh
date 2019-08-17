3c472a3b8a56071677e1bdcdb60cf0833e6b9dff
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-17 14:22:34 +0200

    fix: Use set-env instead of setenv

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo "$GCLOUD_AUTH" | base64 --decode > "$GITHUB_WORKSPACE"/gcloud.json
-setenv GOOGLE_APPLICATION_CREDENTIALS "$GITHUB_WORKSPACE/gcloud.json"
+set-env GOOGLE_APPLICATION_CREDENTIALS "$GITHUB_WORKSPACE/gcloud.json"
 sh -c "gcloud auth activate-service-account --key-file=$GITHUB_WORKSPACE/gcloud.json $*"
