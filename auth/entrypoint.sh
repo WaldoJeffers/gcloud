962e036f6025a0da63cd1c64b6798436e34b2afb
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-26 17:43:31 +0200

    fix: set env variable

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -3,4 +3,6 @@
 set -e
 
 echo "$GCLOUD_AUTH" | base64 --decode > "$GITHUB_WORKSPACE"/gcloud.json
+# Change to echo "::[set-env name=FOO;]BAR" when the line below stops working
+echo "##[set-env name=GOOGLE_APPLICATION_CREDENTIALS;]$GITHUB_WORKSPACE/gcloud.json"
 sh -c "gcloud auth activate-service-account --key-file=$GITHUB_WORKSPACE/gcloud.json $*"
